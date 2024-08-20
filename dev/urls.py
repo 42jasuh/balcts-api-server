662f2e2c12e3cf86763d9d0d537d1e4c235d86ba
woodstock93 <woodstock93@naver.com>
2024-08-20 15:00:17 +0900

    feat(crud): add initial CRUD for Django server

---
+++ dev/urls.py
@@ -0,0 +1,12 @@
+# urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
+
+router = DefaultRouter()
+
+router.register('post', PostViewSet)
+
+urlpatterns =[
+    path('', include(router.urls))
+]
